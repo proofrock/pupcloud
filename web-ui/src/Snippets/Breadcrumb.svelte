811982e6281e0fb8b11d87730bfe17585fc2f96c
Germano Rizzo <germano.rizzo@gmail.com>
2022-03-19 14:53:06 +0100

    Different breadcrumb strategies according to screen size

--- web-ui/src/Snippets/Breadcrumb.svelte
+++ web-ui/src/Snippets/Breadcrumb.svelte
@@ -16,8 +16,8 @@
      * along with PupCloud.  If not, see <http://www.gnu.org/licenses/>.
      */
 
-    import {onMount, onDestroy, createEventDispatcher} from "svelte";
-    import {Dropdown, destroy} from "axentix";
+    import {createEventDispatcher, onDestroy, onMount} from "svelte";
+    import {destroy, Dropdown} from "axentix";
 
     export let path: string[] = [];
 
@@ -40,7 +40,22 @@
     }
 </script>
 
-<div class="dropdown dd-fix" id="Breadcrumb">
+<div class="hide-xs">
+    <!-- svelte-ignore a11y-missing-attribute -->
+    📍
+    {#if path.length > 0}
+        <a on:click={goto(-1)} class="font-w300 cursor-pointer"><u><i>root</i></u></a>
+    {/if}&nbsp;/&nbsp;
+    {#each path as pItem, idx}
+        {#if idx < path.length - 1}
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a on:click={goto(idx)} class="font-w300 cursor-pointer"><u>{pItem.replace('/', '')}</u></a>
+        {:else}
+            <span class="font-w600">{pItem.replace('/', '')}</span>
+        {/if}&nbsp;/&nbsp;
+    {/each}
+</div>
+<div class="dropdown dd-fix hide-sm-up" id="Breadcrumb" style="top: -8px;">
     <div class="navbar-link" data-target="Breadcrumb" title="Folder stack" style="height: 40px;">
         📍&nbsp;<span class="triangle"></span>
     </div>
@@ -57,11 +72,12 @@
         {#each path as pItem, idx}
             {#if idx < path.length - 1}
                 <div class="dropdown-item">
-                    &nbsp;/<span class="pup-a-nobold cursor-pointer" on:click={goto(idx)}>{pItem.replace("/", "")}</span>
+                    &nbsp;/&nbsp;<span class="pup-a-nobold cursor-pointer"
+                                       on:click={goto(idx)}>{pItem.replace("/", "")}</span>
                 </div>
             {:else}
                 <div class="dropdown-item">
-                    &nbsp;/{pItem.replace("/", "")}
+                    &nbsp;/&nbsp;{pItem.replace("/", "")}
                 </div>
             {/if}
         {/each}
