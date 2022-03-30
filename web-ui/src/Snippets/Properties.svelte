023f45a4b066c3dc6defae0d52c3433633a29660
Germano Rizzo <germano.rizzo@gmail.com>
2022-03-30 19:29:19 +0200

    Fixes to UI: icons weren't displayed in some parts of the UI

--- web-ui/src/Snippets/Properties.svelte
+++ web-ui/src/Snippets/Properties.svelte
@@ -144,7 +144,7 @@
 
 <div class="modal shadow-1 white rounded-3 modal-bouncing" id="modal-properties">
     <div class="modal-header ellipsis">
-        <img alt={item.icon} src="icons/48x48/{item.icon}.svg"/>&nbsp;{item.name}
+        <img alt={item.icon[0]} src="icons/48x48/{item.icon[0]}.svg"/>&nbsp;{item.name}
     </div>
     <div class="modal-content container">
         <p class="ellipsis"><b>File name:</b> <span title={item.name}>{item.name}</span></p>
